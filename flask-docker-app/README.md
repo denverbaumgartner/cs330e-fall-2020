2c3a2a48e1c37ea080e7f66442846e3cd52169fb
Fares Fraij <fares@utexas.edu>
2020-08-21 13:20:01 -0500

    uploading additional material

---
+++ flask-docker-app/README.md
@@ -0,0 +1,3 @@
+This project provides all the necessary files to build a simple app using Docker, Flask, and Postgres.
+
+Check out the [wiki page](https://gitlab.com/mmarabella/flask-docker-app/wikis/Tutorial) for detailed tutorial.
---
+++ flask-docker-app/flask-docker-postgres/app/app.py
@@ -0,0 +1,60 @@
+import time
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+DBUSER = 'postgres'
+DBPASS = 'password123'
+DBHOST = 'db'
+DBPORT = '5432'
+DBNAME = 'testdb'
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+    'postgresql+psycopg2://{user}:{passwd}@{host}:{port}/{db}'.format(
+        user=DBUSER,
+        passwd=DBPASS,
+        host=DBHOST,
+        port=DBPORT,
+        db=DBNAME)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = 'password123'
+
+db = SQLAlchemy(app)
+
+class Student(db.Model):
+  id = db.Column('student_id', db.Integer, primary_key=True)
+  name = db.Column(db.String(50))
+  eid = db.Column(db.String(50))
+  major = db.Column(db.String(50))
+
+  def __init__(self, name, eid, major):
+    self.name = name
+    self.eid = eid
+    self.major = major
+
+def insert_students():
+  db.create_all()
+  student1 = Student('John', 'abc123', 'MIS')
+  student2 = Student('Mary', 'asd123', 'Economics')
+  db.session.add(student1)
+  db.session.add(student2)
+  # db.session.rollback()
+  db.session.commit()
+
+@app.route('/')
+def home():
+  students = db.session.query(Student).all()
+  return render_template('Students.html', students = students)
+
+if __name__ == '__main__':
+    dbstatus = False
+    while dbstatus == False:
+        try:
+            db.drop_all()
+            db.create_all()
+        except:
+            time.sleep(2)
+        else:
+            dbstatus = True
+    insert_students()
+    app.run(debug=True, host='0.0.0.0')
